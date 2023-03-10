66c8feb69b8ec9fb6e5bd2d39c5b9f8a27699c8f
markers16123 <markers16123@gmail.com>
2023-03-10 10:39:09 +0900

    프로세스 템플릿 처리 중

---
+++ src/Core/Eum.gRPC.Server.ServiceDesk/Modules/Process/Repositories/StepElementRepository.cs
@@ -0,0 +1,31 @@
+﻿using Eum.Core.Data;
+using Eum.ServiceClient.Contracts.ServiceDesk.Data.Case;
+
+namespace Eum.gRPC.Server.ServiceDesk.Modules.Process.Repositories
+{
+    public interface IStepElementRepository : IRepository
+    {
+    }
+
+    public class StepElementRepository : DatabaseRepositoryBase, IStepElementRepository
+    {
+        public StepElementRepository(IConfiguration configuration) : base("EumServiceDesk", configuration)
+        {
+        }
+
+        public void Set()
+        {
+
+        }
+
+        public void Get()
+        {
+
+        }
+
+        public void Del()
+        {
+
+        }
+    }
+}
---
+++ src/Core/Eum.gRPC.Server.ServiceDesk/Modules/Process/Services/TemplateService.cs
@@ -0,0 +1,11 @@
+﻿using Eum.Core.Data;
+
+namespace Eum.gRPC.Server.ServiceDesk.Modules.Process.Services
+{
+    public interface ITemplateService : IService 
+    {
+    }
+    public class TemplateService : ITemplateService
+    {
+    }
+}
