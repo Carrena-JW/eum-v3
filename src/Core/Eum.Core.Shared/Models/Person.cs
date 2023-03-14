a94dc5e3c1cd5e0232e849fb9b594379fce0d9f1
markers16123 <markers16123@gmail.com>
2023-03-14 18:32:03 +0900

    grpc 응답에 사용자 정보가 모두 null로 표시되는 증상 수정

--- src/Core/Eum.Core.Shared/Models/Person.cs
+++ src/Core/Eum.Core.Shared/Models/Person.cs
@@ -5,16 +5,27 @@ namespace Eum.Core.Shared.Models
     [DataContract]
     public class Person
     {
+        [DataMember(Order = 1)]
         public string PersonCode { get; set; }
+        [DataMember(Order = 2)]
         public string Email { get; set; }
+        [DataMember(Order = 3)]
         public string PersonId { get; set; }
+        [DataMember(Order = 4)]
         public string DisplayName { get; set; }
+        [DataMember(Order = 5)]
         public string EmployeeNumber { get; set; }
+        [DataMember(Order = 6)]
         public string ComCode { get; set; }
+        [DataMember(Order = 7)]
         public string DeptCode { get; set; }
+        [DataMember(Order = 8)]
         public string DeptName { get; set; }
+        [DataMember(Order = 9)]
         public string TitleCode { get; set; }
+        [DataMember(Order = 10)]
         public string TitleName { get; set; }
+        [DataMember(Order = 11)]
         public string Password { get; set; }
     }
 }
